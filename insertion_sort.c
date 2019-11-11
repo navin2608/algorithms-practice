d078a3798bec84d8a8d8c8f343c91025c08d703c
navin2608 <msnsnavin@gmail.com>
2019-11-11 18:07:19 +0530

    corrected

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int a[10]={8,6,3,2,1};
-    int i=0,j=0,key=0;
-    for (i=1;i<5;i++)
+    int a[]={6,5,3,2,1,0,6,6};
+    int n=sizeof(a)/sizeof(a[0]),i,j,key;
+    for(i=1;i<n;i++)
+
     {
         key=a[i];
         j=i-1;
-        while(j>=0 && a[j]>key)
+        while(j>=0 & a[j]>key)
         {
             a[j+1]=a[j];
             j=j-1;
         }
         a[j+1]=key;
     }
-            for(i=0;i<5;i++)
-            printf("%d",a[i]);
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
     }
-
+    }
